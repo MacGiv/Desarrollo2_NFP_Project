543bffe11cae253699552c309011ea986930165a
MacGiv <tomasluchelli.17@gmail.com>
2025-05-25 11:48:49 -0300

    Add Player FSM

---
+++ Nullframe Protocol Project/Assets/Scripts/State Machine/PlayerStateMachine.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+/// <summary>
+/// Controls the current State of the Player and transitions to different states.
+/// </summary>
+public class PlayerStateMachine
+{
+    public PlayerState CurrentState { get; private set; }
+
+    public void Initialize(PlayerState startState)
+    {
+        CurrentState = startState;
+        CurrentState.Enter();
+    }
+
+    public void ChangeState(PlayerState newState)
+    {
+        CurrentState.Exit();
+        CurrentState = newState;
+        CurrentState.Enter();
+    }
+
+    public void Update()
+    {
+        CurrentState?.LogicUpdate();
+    }
+
+    public void FixedUpdate()
+    {
+        CurrentState?.PhysicsUpdate();
+    }
+}
