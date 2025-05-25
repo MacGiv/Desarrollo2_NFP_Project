51eb98c4c3159e754476e81ff914de671130ab29
MacGiv <tomasluchelli.17@gmail.com>
2025-05-25 11:51:22 -0300

    Add PlayerCore script

---
+++ Nullframe Protocol Project/Assets/Scripts/PlayerCore.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class PlayerCore : MonoBehaviour
+{
+    [SerializeField] private PlayerData data;
+
+    // Cached Components
+    private PlayerInputHandler input;
+    private PlayerGroundChecker groundChecker;
+    private PlayerMovement movement;
+    private PlayerStateMachine stateMachine;
+
+    // State Variables
+    private PlayerIdleState state_idle;
+    private PlayerMoveState state_move;
+    private PlayerJumpState state_jump;
+    private PlayerInAirState state_inAir;
+
+    // Properties (for public access)
+    public PlayerIdleState IdleState => state_idle;
+    public PlayerMoveState MoveState => state_move;
+    public PlayerJumpState JumpState => state_jump;
+    public PlayerInAirState InAirState => state_inAir;
+
+    public PlayerInputHandler Input => input;
+    public PlayerMovement Movement => movement;
+    public PlayerGroundChecker GroundChecker => groundChecker;
+
+    private void Awake()
+    {
+        input = GetComponent<PlayerInputHandler>();
+        groundChecker = GetComponent<PlayerGroundChecker>();
+        movement = GetComponent<PlayerMovement>();
+        movement.SetData(data);
+        stateMachine = new PlayerStateMachine();
+
+        // Initialize States
+        state_idle = new PlayerIdleState(this, stateMachine, data);
+        state_move = new PlayerMoveState(this, stateMachine, data);
+        state_jump = new PlayerJumpState(this, stateMachine, data);
+        state_inAir = new PlayerInAirState(this, stateMachine, data);
+
+        stateMachine.Initialize(state_idle);
+    }
+
+    private void Update()
+    {
+        stateMachine.Update();
+    }
+
+    private void FixedUpdate()
+    {
+        groundChecker.CheckGround();
+        movement.UpdateGrounded(groundChecker.IsGrounded);
+        stateMachine.FixedUpdate();
+    }
+}
