28de6dd9a32bd0d93f34c6cb6d2887db59e90dde
MacGiv <tomasluchelli.17@gmail.com>
2025-05-26 07:26:26 -0300

    Add Player Health System

---
+++ Nullframe Protocol Project/Assets/Scripts/PlayerHealthSystem.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PlayerHealthSystem : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 100;
+
+    [SerializeField] private int currentHealth;
+    private bool isGodMode = false;
+    private bool isDead = false;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (!isGodMode || !isDead)
+        {
+            currentHealth -= amount;
+            currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+            Debug.Log("Player took damage. Amount: " + amount);
+            if (currentHealth <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        if (!isDead)
+        {
+            currentHealth += amount;
+            currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        }
+    }
+
+    public void ToggleGodMode()
+    {
+        isGodMode = !isGodMode;
+    }
+
+    private void Die()
+    {
+        isDead = true;
+        Debug.Log("Player has died.");
+        // TODO: Reiniciar escena
+    }
+}
