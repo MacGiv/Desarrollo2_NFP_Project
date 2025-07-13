87f477984c0234af1f14322aa74b9257a773a9ae
MacGiv <tomasluchelli.17@gmail.com>
2025-07-13 01:25:35 -0300

    Update Menu Logic

--- Nullframe Protocol Project/Assets/Scripts/UI/MainMenuUI.cs
+++ Nullframe Protocol Project/Assets/Scripts/UI/MainMenuUI.cs
@@ -1,9 +1,25 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
     [SerializeField] private string startLevelSceneName = "GameScene_1";
     [SerializeField] private string creditsSceneName = "Credits";
+    [SerializeField] private Button ButtonfirstSelected;
+
+    private void OnEnable()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        EventSystem.current.SetSelectedGameObject(ButtonfirstSelected.gameObject);
+    }
+
+    private void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null)
+            EventSystem.current.SetSelectedGameObject(ButtonfirstSelected.gameObject);
+    }
 
     public void StartGame()
     {
