5a816a86b0a5e1612d47116312f454ceb8c806fe
MacGiv <tomasluchelli.17@gmail.com>
2025-07-18 23:59:13 -0300

    Project partial cleanup

--- Nullframe Protocol Project/Assets/Scripts/Scene Management/SceneLoader.cs
+++ Nullframe Protocol Project/Assets/Scripts/Scene Management/SceneLoader.cs
@@ -35,32 +35,3 @@ public class SceneLoader : MonoBehaviour
             yield return null;
     }
 }
-
-
-/*
-
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class SceneLoader : MonoBehaviour
-{
-    [SerializeField] public enum scenesIndex {MAIN_MENU, CREDITS, GAMEPLAY_1, GAMEPLAY_2}
-    
-    public void LoadScene(int sceneIndex)
-    {
-        SceneManager.LoadScene(sceneIndex);
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
-
-// FOR DEBUG ONLY
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#endif
-// FOR DEBUG ONLY
-    }
-}
-
-*/
